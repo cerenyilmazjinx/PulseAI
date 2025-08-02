82c950959dcc2eb1dfd0e141631b652b1b6e5709
Cereny <56936296+cerenyilmazjinx@users.noreply.github.com>
2025-08-02 19:52:20 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/pulseai/main.go
@@ -0,0 +1,23 @@
+// cmd/pulseai/main.go
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"pulseai/internal/pulseai"
+)
+
+func main() {
+	verbose := flag.Bool("verbose", false, "Enable verbose logging")
+	input := flag.String("input", "", "Input file path")
+	output := flag.String("output", "", "Output file path")
+	flag.Parse()
+
+	app := pulseai.NewApp(*verbose)
+	if err := app.Run(*input, *output); err != nil {
+		log.Printf("Error: %v", err)
+		os.Exit(1)
+	}
+}
